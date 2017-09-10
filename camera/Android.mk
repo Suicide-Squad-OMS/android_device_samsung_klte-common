ac97e48c967b1830f7394d4ae0618cb4b992e992
Tobias Gunkel <hennymcc@yahoo.de>
2017-09-10 19:03:56 +0200

    camera: fix compilation for lineageos-15.0
    Change-Id: I445bd12c6c7e0fe1c8e969b17ff78111dd629aee
    
    # Conflicts:
    #	camera/Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -2,16 +2,19 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
-    system/media/camera/include
+    system/media/camera/include \
+    system/core/base/include \
+    frameworks/native/libs/arect/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
-LOCAL_STATIC_LIBRARIES := libbase libarect
+
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl \
+    libhardware liblog libcamera_client libutils \
     android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
+
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
