d7974c0d2c9e0182e2269e7d37931a9471e89bb0
BlackDeath <BlackDeaths@users.noreply.github.com>
2017-10-21 10:58:23 +0200

    samsung
    servicesmode

--- klte.mk
+++ klte.mk
@@ -128,6 +128,10 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/hidl/manifest.xml:system/vendor/manifest.xml
+	
+# Samsung
+PRODUCT_PACKAGES += \
+    SamsungServiceMode	
 
 # Ramdisk
 PRODUCT_PACKAGES += \
