14cbc74f254081d336b09edd6c52605762e71dc8
BlackDeath <BlackDeaths@users.noreply.github.com>
2017-10-23 21:08:42 +0200

    klte-common: Use opensource ril
    git fetch
    https://review.lineageos.org/LineageOS/android_device_samsung_klte-common
    refs/changes/97/194097/10 && git cherry-pick FETCH_HEAD

--- system_prop.mk
+++ system_prop.mk
@@ -57,7 +57,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.add_power_save=1 \
     persist.radio.lte_vrat_report=1 \
     ro.telephony.mms_data_profile=5 \
-    ro.telephony.ril_class=KlteRIL
+    ro.ril.telephony.mqanelements=6
 
 # Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
 PRODUCT_PROPERTY_OVERRIDES += \
